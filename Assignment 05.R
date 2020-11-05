afccdc66037c55853e373701328b1fa2fdd6fbd9
Enzo Zechiel <egz224@lehigh.edu>
2020-11-05 15:27:19 -0500

    Assignment 05.R

--- Assignment 05.R
+++ Assignment 05.R
@@ -1,19 +1,24 @@
 importIntoEnv(USvideos)
 View(USvideos)
 
-USvid <- read.csv("USvideos.csv")
-if(!require("tidyverse")) install.packages("tidyverse")
+library(stringr)
 library(tidyverse)
+
+USvid <- read.csv("USvideos.csv")
+attach(USvid)
+if(!require("tidyverse")) install.packages("tidyverse")
 
 
 USvid$publish_time <- striptime(USvid$publish_time, format = "%Y-%m-%dT%H:%M:%OS", tz = "UTC")
 library(dplyr)
 
-UStidy <- USvid %>% seperate(col=publish_time,into =c("PDate", "PTime"), extra="merge")
-View(UStidy)
+UStidy <- USvid %>% seperate(col=publish_time,into =c("PDate", "PTime"), "T" extra="merge")
+Ustidy$PDate <- as.Date(UStidy$PDate)
 
 
 UStidy$trending_date <- as.Date(UStidy$trending_date, "%y.%d.%m")
+
+UStidy$title <- str_to_title(title)
 
 View(UStidy)
 
